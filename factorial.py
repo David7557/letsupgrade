474ce5bf2d580c1eee1d4b5dd61ef246d50e44a6
David Kumar Hansdah <102952845+David7557@users.noreply.github.com>
2023-03-04 21:22:54 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num = 5
+
+factorial = 1
+
+if num < 0:
+   print("Factorial doesnot exist.")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("Factorial is = ",factorial)
